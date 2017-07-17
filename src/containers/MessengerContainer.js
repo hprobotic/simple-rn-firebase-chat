96e039308408250eaa075d416bc21bb282ca323d
JohnP <hprobotic@gmail.com>
2017-07-17 19:44:40 +0700

    update

--- src/containers/MessengerContainer.js
+++ src/containers/MessengerContainer.js
@@ -19,12 +19,12 @@ if (Platform.OS == 'ios') {
   var STATUS_BAR_HEIGHT = 0;
   var CONTAINER_MARGIN = 20;
   var UserName = 'ios';
-  var AvatarUrl = 'https://source.unsplash.com/sseiVD2XsOk/100x100';
+  var AvatarUrl = 'https://unsplash.it/100/100/?random';
 } else {
   var STATUS_BAR_HEIGHT = 27;
   var CONTAINER_MARGIN = 0;
   var UserName = 'android';
-  var AvatarUrl = 'https://source.unsplash.com/2Ts5HnA67k8/100x100';
+  var AvatarUrl = 'https://unsplash.it/100/100/?random';
 }
 
 class MessengerContainer extends Component {
